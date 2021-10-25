5f9eb4f2d7c2f389897e1217c76d8d6a7a74b0a0
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2021-10-25 14:46:43 -0700

    Save pairwise comparison p-values, boxplots

--- KW_and_Boxplots.R
+++ KW_and_Boxplots.R
@@ -8,9 +8,10 @@ library(dunn.test)
 # KW test & boxplots to compare months ------------------------------------
 
 inDir = 'I:/TimeSeries'
-dielDir = 'I:/Diel Plots'
+seasDir = 'I:/Seasonal_Plots'
+int = "Daily"
 
-fileList = list.files(path=inDir,pattern='*.csv',
+fileList = list.files(path=inDir,pattern=paste('*_',int,'.csv',sep=""),
                       full.names=TRUE,recursive=FALSE,
                       include.dirs=FALSE,no..=TRUE)
 
@@ -22,13 +23,13 @@ for (i in 1:numel(fileList)){  # for each species file
   sites = attribs$names[2:numel(attribs$names)]
   
   CTname = str_remove(fileList[i],paste(inDir,'/',sep="")) # get the species/CT name
-  CTname = str_remove(CTname,".csv")
+  CTname = str_remove(CTname,paste('_',int,'.csv',sep=""))
   
   dateVec = as.Date(thisCT$Date,format="%d-%b-%Y")
   
   # if it doesn't already exist, create directory to save figures
-  if (!dir.exists(paste(dielDir,'/',CTname,sep=""))){
-    dir.create(paste(dielDir,'/',CTname,sep=""))
+  if (!dir.exists(paste(seasDir,'/',CTname,sep=""))){
+    dir.create(paste(seasDir,'/',CTname,sep=""))
   }
   
   for (j in 1:numel(sites)){ # for each site
@@ -45,26 +46,30 @@ for (i in 1:numel(fileList)){  # for each species file
       
       # test for significant differences between months
       kruskal.test(thisSite,monthGroup,na.action=na.pass) 
-      # pairComp = pairwise.wilcox.test(thisSite,monthGroup,p.adjust.method="BH") # don't use, doesn't use rankings or pool variance in same way as KW test
-      pairCompCI = conover.test(thisSite,monthGroup,method='bonferroni',wrap=TRUE,
-                              table=TRUE,alpha=0.05)
-      pairCompDunn = dunn.test(thisSite,monthGroup,method='bonferroni',wrap=TRUE,
-                           table=TRUE,alpha=0.05)
+      pairCompCI = conover.test(thisSite,monthGroup,method='bonferroni',label=TRUE,
+                                wrap=TRUE,table=TRUE,alpha=0.05)
+      # pairCompDunn = dunn.test(thisSite,monthGroup,method='bonferroni',wrap=TRUE,
+      #                      table=TRUE,alpha=0.05)
+      CITable = matrix(NA,11,11)
+      CITable[upper.tri(CITable,diag=TRUE)] = t(pairCompCI$P.adjusted)
+      CITable = t(CITable)
+      rownames(CITable) = c("10","11","12","2","3","4","5","6","7","8","9")
+      colnames(CITable) = c("1","10","11","12","2","3","4","5","6","7","8")
+      saveName = paste(seasDir,'/',CTname,'/',sites[j],"_Monthly_CI_Comparisons.csv",sep="")
+      write.csv(CITable,file=saveName,row.names=TRUE)
       
       # Identify which pairwise comparisons were significant
-      sig = which(pairCompCI$P.adjusted<0.05)
-      sigPairs = pairCompCI$comparisons[sig]
-      sigPairs = str_replace(sigPairs," - "," ")
-      sigPairs = as.numeric(sigPairs)
+      # sig = which(pairCompCI$P.adjusted<0.025)
+      # sigPairs = pairCompCI$comparisons[sig]
+      # sigNumbers = regmatches(sigPairs, gregexpr("[[:digit:]]+", sigPairs))
+      # sigNumbers = as.numeric(unlist(sigNumbers))
       
       # Plot data by month
-      par(mfrow=c(2,1))
-      
+      saveName = paste(seasDir,'/',CTname,'/',sites[j],"_MonthlyBoxplot.png",sep="")
+      png(saveName)
       boxplot(thisSite~monthGroup,main=paste(paste(CTname,'at',sites[j])),
               xlab=c("Month"),ylab=c("Daily Counts"))
-      # p = ggplot(data.frame(thisSite, as.factor(monthGroup)),aes(x=monthGroup, y=thisSite))
-      # p + geom_violin()
-      
+      dev.off()
       
     }
 
